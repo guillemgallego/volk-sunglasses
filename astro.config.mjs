5195ac5f861579d212b0f4fed6fb5bc15b22bb29
guillemgallego <guillemgallego@users.noreply.github.com>
2026-04-13 22:18:36 +0200

    Fix: Configurado Astro SSR para la API del Chatbot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://volksunglasses.com',
-  output: 'static',
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
